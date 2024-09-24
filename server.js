50deb50319afe9bdacbabe2b25e5ce2a5349aca0
Azamat Gaparov <agaparovkg@gmail.com>
2024-09-23 19:46:49 -0700

    new changes

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 // ESM
 import express from "express";
+import { users } from "./data.js";
+
+console.log(users);
 // CommonJS
 // const express = require("express");
 
@@ -16,7 +19,11 @@ const app = express();
 const port = process.env.PORT || 8000;
 
 app.get("/", async (req, res) => {
-  res.send("Welcome to Homepage");
+  res.send({
+    message: "Welcome to Homepage",
+    total_Users: users.length,
+    users: users,
+  });
 });
 
 app.listen(port, () => {
